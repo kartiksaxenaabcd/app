ca3a790fb5e98e87e8bdcb6643a18090f32c4fba
Kartik Saxena <kartiksaxenaabcd@gmail.com>
2022-10-01 13:42:28 +0530

    added

---
+++ android/app/src/main/kotlin/com/stonks/mood/mood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.stonks.helm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+library constants;
+
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = MaterialColor(0xffffd98e, <int, Color>{});
+const ACCENT_COLOR = MaterialColor(0xff314675, <int, Color>{});
+
+const DATABASE_NAME = 'helm';
